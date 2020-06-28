3de37cbde891d4ac1b2e38fde7d3403ad827025a
Gauri-tech <gauridabhade32@gmail.com>
2020-06-28 12:58:42 +0530

    second day ouput

--- BasicCoreJava/src/ifElseStatements/ifElseStatementLogicalOperator.java
+++ BasicCoreJava/src/ifElseStatements/ifElseStatementLogicalOperator.java
@@ -4,7 +4,27 @@ public class ifElseStatementLogicalOperator {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+ int age=18;
+ boolean voterId=true;
+   if(age>=18 && voterId)
+   {
+	   System.out.println("you are eligibe");
+   }
+	
+	else {
+		System.out.println(" not eligible");
 	}
+	
 
-}
+
+	// or operator
+  boolean panCard=true;
+  boolean aadharCard=true;
+  
+  if(panCard || aadharCard)
+  { System.out.println("You are eligible");
+  }
+  else {System.out.println("You are not eligible");
+  }
+  }
+  }
